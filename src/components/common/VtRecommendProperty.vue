8c9f0ccb490448f315a7d4d2cfef1ad71aaf6f39
steven <steven.pilagan@gmail.com>
2020-07-24 09:30:07 +0800

    main - made recommended property card clickable

--- src/components/common/VtRecommendProperty.vue
+++ src/components/common/VtRecommendProperty.vue
@@ -2,12 +2,14 @@
   <div class="recommend-properties">
     <h2 class="text-8 font-400 mb-8">Nearby places to stay</h2>
     <div class="grid grid-cols-12 gap-4">
-      <VtRecommendPropertyCard
+      <router-link
         class="col-span-3"
+        :to="'/property/' + property.name.replace(/ /g, '-').toLowerCase()"
         v-for="(property, index) in properties"
         :key="index"
-        :property="property"
-      />
+      >
+        <VtRecommendPropertyCard :property="property"/>
+      </router-link>
     </div>
   </div>
 </template>
